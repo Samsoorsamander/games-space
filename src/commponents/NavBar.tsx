cc95d18b11ecf344c449ca8f7bd615f56a685078
Samsoorsamander <samsoorsamander@gmail.com>
2024-07-06 20:24:08 -0800

    Build Nav Bar

--- src/commponents/NavBar.tsx
+++ src/commponents/NavBar.tsx
@@ -1,11 +1,12 @@
-import { HStack } from "@chakra-ui/react"
-
-
+import { HStack, Image, Text } from "@chakra-ui/react";
+import RLogo from "../assets/randomeLogo.webp.ico";
 const NavBar = () => {
   return (
     <HStack>
-        
+      <Image src={RLogo} />
+      <Text>NavBar</Text>
     </HStack>
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
