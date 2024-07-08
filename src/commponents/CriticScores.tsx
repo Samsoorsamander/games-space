0e7a03d9a7e7fcddb7d9ca904967c14ce97225b0
Samsoorsamander <samsoorsamander@gmail.com>
2024-07-07 21:27:59 -0800

    Displying Critic Scores

---
+++ src/commponents/CriticScores.tsx
@@ -0,0 +1,18 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScores = ({ score }: Props) => {
+  let color = score > 75 ? "green" : score > 65 ? "yellow" : "";
+  return (
+    <>
+      <Badge fontSize='14px' colorScheme={color} borderRadius="4px" paddingX={2}>
+        {score}
+      </Badge>
+    </>
+  );
+};
+
+export default CriticScores;
--- src/commponents/GameCards.tsx
+++ src/commponents/GameCards.tsx
@@ -1,6 +1,7 @@
-import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconsList from "./PlatformIconsList";
+import CriticScores from "./CriticScores";
 
 interface Props {
   game: Game;
@@ -11,9 +12,12 @@ const GameCards = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
-        <PlatformIconsList
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
+        <HStack justifyContent="space-between">
+          <PlatformIconsList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScores score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   );
