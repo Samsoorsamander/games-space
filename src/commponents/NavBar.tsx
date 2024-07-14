2b2e67b34778ce415395eb7d14adfac12ffb31e5
Samsoorsamander <samsoorsamander@gmail.com>
2024-07-14 05:39:33 -0800

    implement searching

--- src/commponents/NavBar.tsx
+++ src/commponents/NavBar.tsx
@@ -1,10 +1,16 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import RLogo from "../assets/randomeLogo.webp.ico";
 import ColorModeSwitch from "./ColorModeSwitch";
-const NavBar = () => {
+import SearchInput from "./SearchInput";
+
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+const NavBar = ({ onSearch }: Props) => {
   return (
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack padding="10px">
       <Image src={RLogo} />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -19,7 +19,11 @@ export  interface Game {
 
 
 const useGames = (gameQuery: GameQuery) => useData<Game>('/games', {params: {
-  genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id, ordering:gameQuery.sortOrder}},
+  genres: gameQuery.genre?.id,
+   platforms: gameQuery.platform?.id, 
+   ordering:gameQuery.sortOrder,
+  search:gameQuery.searchText
+  }},
    [gameQuery])
 
 export default useGames;
