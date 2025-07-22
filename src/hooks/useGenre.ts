23b9188c7832eec5564ec9c5ee2df2657690d5a7
S@msoor Samander <154373431+Samsoorsamander@users.noreply.github.com>
2025-07-21 21:36:19 -0700

    Add react Query

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,14 +1,15 @@
-
-
-import genres from "../Data/genres"
+import { useQuery } from "@chakra-ui/react";
+import genres from "../Data/genres";
 
 export interface Genre {
-    id:number
-    name:string
-    image_background:string
+  id: number;
+  name: string;
+  image_background: string;
 }
+// const useGenre = () => (
 
+// )
 
-const useGenre = () => ({data:genres, isLoading:false, error:null})
+// const useGenre = () => ({data:genres, isLoading:false, error:null})
 
 export default useGenre;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,10 +1,24 @@
-
 import axios from "axios";
 
-export default  axios.create({
-    baseURL:'https://api.rawg.io/api',
-    params: {
-        key:'6e9e1dcad000475490d088908fb322b1',
-    }
-})  
+const axiosInstance = axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "6e9e1dcad000475490d088908fb322b1",
+  },
+});
 
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = () => {
+    return axiosInstance.get<T>(this.endpoint).then((res) => res.data);
+  };
+
+  post = (data: T) => {
+    return axiosInstance.post<T>(this.endpoint, data).then((res) => res.data);
+  };
+}
